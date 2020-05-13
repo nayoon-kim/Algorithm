b4fc9c0c92fb7789551a9cf7fda03ed7e3e7ab47
nayoon-kim <belloyv@gmail.com>
2020-05-13 20:31:17 +0900

    Update 벽 부수고 이동하기.py
    pypy3

--- 벽 부수고 이동하기.py
+++ 벽 부수고 이동하기.py
@@ -1,25 +1,31 @@
 import queue
+import copy
+
+answer = False
+
+# point 각각의 경로에 대해서 방문여부를 기록한다.
 
 def bfs(q, str, n, m):
     dir = [[1, 0], [-1, 0], [0, 1], [0, -1]]
     global answer
 
     _q = queue.Queue()
-    while q.empty() != 0:
+    while q.empty() == False:
         p = q.get()
         for d in dir:
             if p[0] + d[0] == n - 1 and p[1] + d[1] == m - 1:
                 answer = True
             if p[0] + d[0] > -1 and p[0] + d[0] < n and p[1] + d[1] > -1 and p[1] + d[1] < m:
-                if str[p[0] + d[0]][p[1] + d[1]] == 0:
-                    _q.put([p[0]+d[0], p[1]+d[1], p[2]])
-                if str[p[0] + d[0]][p[1] + d[1]] == 1:
-                    if p[2] == 0:
-                        _q.put([p[0]+d[0], p[1]+d[1], p[2]])
-                print(p[0] + d[0],p[1] + d[1])
+                if p[3][p[0]+d[0]][p[1]+d[1]] == 0:
+                    p[3][p[0]+d[0]][p[1]+d[1]] = 1
+                    if str[p[0] + d[0]][p[1] + d[1]] == 0:
+                        _q.put([p[0]+d[0], p[1]+d[1], p[2], p[3]])
+                    if str[p[0] + d[0]][p[1] + d[1]] == 1:
+                        if p[2] == 0:
+                            _q.put([p[0]+d[0], p[1]+d[1], p[2] + 1, p[3]])
     return _q
 
-answer = False
+
 a = input().split(' ')
 n, m = int(a[0]), int(a[1])
 
@@ -31,10 +37,12 @@ for i in range(0, n):
     for j in range(0, m):
         str[i][j] = int(str[i][j])
 
-q = queue.Queue()
-q.put([0, 0, 0])
+temp = [[0] * m for i in range(0, n)]
 
-min = 0
+q = queue.Queue()
+q.put([0, 0, 0, temp])
+
+min = 1
 
 while True:
     if answer:
@@ -42,4 +50,8 @@ while True:
         break
     else:
         q = bfs(q, str, n, m)
+
+        if q.empty() == True:
+            print(-1)
+            break
         min+=1
