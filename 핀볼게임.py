b247593c6a8c1f96b73a57045e18703d3e715b1c
nayoon-kim <belloyv@gmail.com>
2020-06-05 17:48:29 +0900

    direction을 토대로 바꾸고 있음 근데 벽에 튕겨나왔는데 바로 블록이 있으면 어떻게 처리할 것인가를 고민하셈

--- 핀볼게임.py
+++ 핀볼게임.py
@@ -1,84 +1,129 @@
 # ↑ = 0, ↓ = 1, → = 2, ← = 3
 def solution(arr, num, index, _index, dir):
     i, j, d = index, _index, dir
-
+    visited = 0
     while True:
-        if i < 0:
-
-        elif i > len(arr) - 1:
-        elif j < 0:
-        elif j > len(arr) - 1:
-
-        if arr[i][j] == -1 or (i == index and j == _index):
-            print(num)
-            break
-        if arr[i][j] == 0:
-            if d == 0:
-                i += 1
-            elif d == 1:
-                i -= 1
-            elif d == 2:
-                j += 1
-            else:
-                j -= 1
-            continue
-        elif arr[i][j] == 1:
-            if d == 0:
-                d = 1
-            elif d == 1:
-                d = 2
-            elif d == 2:
-                d == 3
-            elif d == 3:
-                d = 0
-        elif arr[i][j] == 2:
-            if d == 0:
-                d = 2
-            elif d == 1:
-                d = 0
-            elif d == 2:
-                d == 3
-            elif d == 3:
-                d = 1
-        elif arr[i][j] == 3:
-            if d == 0:
-                d = 3
-            elif d == 1:
-                d = 0
-            elif d == 2:
-                d == 1
-            elif d == 3:
-                d = 2
-        elif arr[i][j] == 4:
-            if d == 0:
-                d = 1
-            elif d == 1:
-                d = 3
-            elif d == 2:
-                d == 0
-            elif d == 3:
-                d = 2
-        elif arr[i][j] == 5:
-            if d == 0:
-                d = 1
-            elif d == 1:
-                d = 0
-            elif d == 2:
-                d == 3
-            elif d == 3:
-                d = 2
-        else:
-            for _a, a in enumerate(arr):
-                for _b, b in enumerate(a):
-                    if arr[i][j] == b and (i != _a or j != _b):
-                        i, j = _a, _b
-                        break
-            continue
 
         if d == 0:
-            i += 1
+            if i < 0:
+                d = 1
+                num += 1
+                i += 1
+                continue
+
+            if arr[i][j] == 1 or arr[i][j] == 4 or arr[i][j] == 5:
+                d = 1
+            elif arr[i][j] == 2:
+                d = 2
+            elif arr[i][j] == 3:
+                d = 3
+            elif arr[i][j] == 0:
+                if i == index and j == _index and visited != 0:
+                    break
+                visited += 1
+                d = 0
+                i -= 1
+                print("value: 0, d == 0, {0},{1}".format(i, j))
+                continue
+            else:
+                for _a, a in enumerate(arr):
+                    for _b, b in enumerate(a):
+                        if arr[i][j] == b and (i != _a or j != _b):
+                            i, j = _a, _b
+                            break
+                continue
+            print("d == 0, {0},{1}".format(i, j))
         elif d == 1:
+            if i > len(arr) - 1:
+                d = 0
+                num += 1
+                i -= 1
+                continue
+
+            if arr[i][j] == 2 or arr[i][j] == 3 or arr[i][j] == 5:
+                d = 0
+            elif arr[i][j] == 0:
+                if i == index and j == _index and visited != 0:
+                    break
+                visited += 1
+                d = 1
+                i += 1
+                print("value: 0, d == 1, {0},{1}".format(i, j))
+                continue
+            elif arr[i][j] == 1:
+                d = 2
+            elif arr[i][j] == 4:
+                d = 3
+            else:
+                for _a, a in enumerate(arr):
+                    for _b, b in enumerate(a):
+                        if arr[i][j] == b and (i != _a or j != _b):
+                            i, j = _a, _b
+                            break
+                continue
+            print("d == 1, {0},{1}".format(i, j))
+        elif d == 2:
+            if j > len(arr) - 1:
+                d = 3
+                num += 1
+                j -= 1
+                continue
+
+            if arr[i][j] == 1 or arr[i][j] == 2 or arr[i][j] == 5:
+                d = 3
+            elif arr[i][j] == 0:
+                if i == index and j == _index and visited != 0:
+                    break
+                visited += 1
+                d = 2
+                j += 1
+                print("value: 0, d == 2, {0},{1}".format(i, j))
+                continue
+            elif arr[i][j] == 3:
+                d = 1
+            elif arr[i][j] == 4:
+                d = 0
+            else:
+                for _a, a in enumerate(arr):
+                    for _b, b in enumerate(a):
+                        if arr[i][j] == b and (i != _a or j != _b):
+                            i, j = _a, _b
+                            break
+                continue
+            print("d == 2, {0},{1}".format(i, j))
+        elif d == 3:
+            if j < 0:
+                d = 2
+                num += 1
+                j += 1
+                continue
+
+            if arr[i][j] == 3 or arr[i][j] == 4 or arr[i][j] == 5:
+                d = 2
+            elif arr[i][j] == 0:
+                if i == index and j == _index and visited != 0:
+                    break
+                visited += 1
+                d = 3
+                j -= 1
+                print("value: 0, d == 3, {0},{1}".format(i, j))
+                continue
+            elif arr[i][j] == 1:
+                d = 0
+            elif arr[i][j] == 2:
+                d = 1
+            else:
+                for _a, a in enumerate(arr):
+                    for _b, b in enumerate(a):
+                        if arr[i][j] == b and (i != _a or j != _b):
+                            i, j = _a, _b
+                            break
+                continue
+            print("d == 3, {0},{1}".format(i, j))
+        if d == 0:
             i -= 1
+        elif d == 1:
+            i += 1
         elif d == 2:
             j += 1
         else:
@@ -86,6 +131,10 @@ def solution(arr, num, index, _index, dir):
 
         num += 1
 
+        if arr[i][j] == -1:
+            break
+    print(num)
+# 벽에 즉각적으로 튕겨나온 핀볼에 대한 처리가 없음
 T = int(input())
 for test_case in range(1, T + 1):
     n = int(input())
@@ -95,19 +144,23 @@ for test_case in range(1, T + 1):
     num = 0
     for index, i in enumerate(arr):
         for _index, j in enumerate(i):
-            for dir in range(0, 4):
-                solution(arr, num, index, _index, dir)
+            if arr[index][_index] == 0:
+                for dir in range(0, 4):
+                    print("{0},{1},{2} go in".format(index, _index, dir))
+                    solution(arr, num, index, _index, dir)
     print("#{0} {1}".format(test_case, num))
 
-# 1
-# 10
-# 0 1 0 3 0 0 0 0 7 0
-# 0 0 0 0 -1 0 5 0 0 0
-# 0 4 0 0 0 3 0 0 2 2
-# 1 0 0 0 1 0 0 3 0 0
-# 0 0 3 0 0 0 0 0 6 0
-# 3 0 0 0 2 0 0 1 0 0
-# 0 0 0 0 0 1 0 0 4 0
-# 0 5 0 4 1 0 7 0 0 5
-# 0 0 0 0 0 1 0 0 0 0
-# 2 0 6 0 0 4 0 0 0 4
+"""
+1
+10
+0 1 0 3 0 0 0 0 7 0
+0 0 0 0 -1 0 5 0 0 0
+0 4 0 0 0 3 0 0 2 2
+1 0 0 0 1 0 0 3 0 0
+0 0 3 0 0 0 0 0 6 0
+3 0 0 0 2 0 0 1 0 0
+0 0 0 0 0 1 0 0 4 0
+0 5 0 4 1 0 7 0 0 5
+0 0 0 0 0 1 0 0 0 0
+2 0 6 0 0 4 0 0 0 4
+"""
