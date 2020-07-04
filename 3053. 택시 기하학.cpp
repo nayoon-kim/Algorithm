c446b4850a00d416b83cffe3d918619ccd340c97
김나윤 <belloyv@gmail.com>
2020-07-04 12:49:36 +0900

    algorithm
    update ~ 7/4

---
+++ 3053. 택시 기하학.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+//const double PI = 3.14159265358979323846264338327950288;
+
+int main() {
+	
+	int n;
+	cin >> n;
+	cout.setf(ios::fixed);
+	cout.precision(6);
+	cout << n * 3.14159265358979323846264338327950288 << endl;
+	cout << (double)2 * n * n << endl;
+	
+}
