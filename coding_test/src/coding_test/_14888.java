26798da3c9408f3a1461c8cb1cd5dc8eaf349762
김나윤 <belloyv@gmail.com>
2021-08-04 23:54:32 +0900

    Create _14888.java

---
+++ coding_test/src/coding_test/_14888.java
@@ -0,0 +1,18 @@
+package coding_test;
+
+import java.util.Scanner;
+
+public class _14888 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] A = new int[N];
+		for(int i = 0; i < N; i++)
+			A[i] = sc.nextInt();
+		int[] op = new int[4];
+		for(int i = 0; i < 4; i++)
+			op[i] = sc.nextInt();
+		
+		
+	}
+}
