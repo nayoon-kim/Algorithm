635bb56edd4daaf3b438e296e1f2f5b5e9159ad0
nayoon <belloyv@gmail.com>
2023-01-24 15:16:10 +0900

    [Codility] Main

---
+++ codility/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Lessons_10_Flags  flags = new Lessons_10_Flags();
+        System.out.println(flags.solution(new int[]{1, 3, 2}));
+    }
+}
