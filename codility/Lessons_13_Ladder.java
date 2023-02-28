f9740ee84704e2bcea06a1f5a27e101b3b4f5c4c
nayoon <belloyv@gmail.com>
2023-02-28 21:57:53 +0900

    100% Ladder

--- codility/Lessons_13_Ladder.java
+++ codility/Lessons_13_Ladder.java
@@ -4,26 +4,40 @@ public class Lessons_13_Ladder {
     public int[] solution(int[] A, int[] B) {
         int size = A.length;
         int[] result = new int[size];
-
-
+        boolean[] visited = new boolean[31];
+        Map<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i < size; i++) {
-            int fib = makeFib(A[i], B[i]);
-            result[i] = fib;
+            if (map.get(B[i]) == null) {
+                map.put(B[i], A[i]);
+            } else {
+                if (map.get(B[i]) < A[i]) {
+                    map.put(B[i], A[i]);
+                }
+            }
+        }
+        for(int i = 0; i < size; i++) {
+            if (visited[B[i]]) continue;
+            visited[B[i]] = true;
+            int[] fib = makeFib(map.get(B[i]), B[i]);
+
+            for(int j = 0; j < size; j++) {
+                if (B[i] != B[j]) continue;
+                result[j] = fib[A[j]];
+            }
         }
         return result;
     }
 
-    public static int makeFib(int a, int b)
+    public static int[] makeFib(int a, int b)
     {
         int modulo = (int)Math.pow(2, b);
-        int t1 = 1;
-        int t2 = 1;
+        int[] fb = new int[a + 1];
+        fb[0] = 1;
+        fb[1] = 1;
         for(int i = 2; i <= a; i++) {
-            int t3 = (t1 + t2) % modulo;
-            t1 = t2;
-            t2 = t3;
+            fb[i] = (fb[i - 1] + fb[i - 2]) % modulo;
         }
 
-        return t2;
+        return fb;
     }
 }
